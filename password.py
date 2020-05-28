36b915942f4994fe5d722198f19787b03a36e638
Jeff Kee <jeffkhm@gmail.com>
2020-05-27 23:06:59 -0700

    update to while 还有> 0

--- password.py
+++ password.py
@@ -1,6 +1,6 @@
 passwords = '38972929'
 还有 = 3 # 剩下三次机会
-while True:
+while 还有 > 0:
 	pwd = input('请输入密码：')
 	if pwd == passwords:
 		print('成功！')
@@ -8,8 +8,7 @@ while True:
 	else:
 		还有 = 还有 - 1
 		print('密码错误! 剩下', 还有 , '次机会')
-		if 还有 == 0:
-			break 
+	
             
            
 		     
